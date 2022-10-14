5861102697a9edc8b1fc11b1f1801f2fbb54f098
Aurimas Č <106968822+AurimasCerniauskas@users.noreply.github.com>
2022-10-14 12:11:16 +0300

    add focus

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { useState } from "react";
 import SaveDate from "../Contexts/SaveDate";
 import Meal from "./Meal";
@@ -7,6 +7,8 @@ function Search() {
   const [text, setText] = useState("");
   const [findRes, setFindRes] = useState(null);
   const { setSaveModalData, recepies } = useContext(SaveDate);
+  const inputField =useRef();
+  const findBtn = useRef();
 
   const searchMeal = () => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${text}`)
@@ -14,13 +16,22 @@ function Search() {
       .then((data) => setFindRes(data))
       .catch((_) => setFindRes("Error"));
     setText("");
+    inputField.current.focus();
   };
+  const handleKeypress = e => {
+    if(e.keyCode === 13){
+      findBtn.current.focus();
+    }
+  }
+  useEffect(()=>{
+    inputField.current.focus();
+  }, [])
 
   return (
     <>
       <div className="row gx-5 mb-4 field-group offset-md-1">
-        <input className="col-sm-6 col-md-4 px-2" placeholder="Search meal by name, e.g 'pie'" value={text} onChange={(e) => setText(e.target.value)}/>
-        <button disabled={text ? false : true} className="col-sm-6 col-md-2 px-2" onClick={searchMeal}>Receptų paieška</button>
+        <input className="col-sm-6 col-md-4 px-2" placeholder="Search meal by name, e.g 'pie'" ref={inputField} value={text} onChange={(e) => setText(e.target.value)} onKeyDown={handleKeypress}/>
+        <button disabled={text ? false : true} className="col-sm-6 col-md-2 px-2" onClick={searchMeal} ref={findBtn}>Receptų paieška</button>
         <button className="btn btn-success col-sm-6 col-md-3 offset-md-1" onClick={() => setSaveModalData(recepies)}>Išsaugoti receptai</button>
       </div>
       <div className="row mb-4">
